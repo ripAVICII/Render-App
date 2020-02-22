645d56bd0ef7ed29778959ae82ba891dfea778f0
James Hilton-Barber <jameshiltonbarber@gmail.com>
2020-02-22 13:18:07 +0100

    updated login

--- Render/LoginViewController.swift
+++ Render/LoginViewController.swift
@@ -7,19 +7,81 @@ Login view controller.
 
 import UIKit
 import AuthenticationServices
+import CryptoKit
+
 
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var loginProviderStackView: UIStackView!
+    fileprivate var currentNonce: String?
+
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupProviderLoginView()
     }
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        performExistingAccountSetupFlows()
+    }
+    
+    @available(iOS 13, *)
+       func startSignInWithAppleFlow() {
+         let nonce = randomNonceString()
+         currentNonce = nonce
+         let appleIDProvider = ASAuthorizationAppleIDProvider()
+         let request = appleIDProvider.createRequest()
+         request.requestedScopes = [.fullName, .email]
+         request.nonce = sha256(nonce)
+
+         let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+         authorizationController.delegate = self
+         authorizationController.presentationContextProvider = self
+         authorizationController.performRequests()
+       }
+
+       @available(iOS 13, *)
+       private func sha256(_ input: String) -> String {
+         let inputData = Data(input.utf8)
+         let hashedData = SHA256.hash(data: inputData)
+         let hashString = hashedData.compactMap {
+           return String(format: "%02x", $0)
+         }.joined()
+
+         return hashString
+       }
+    
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      let charset: Array<Character> =
+          Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+      var result = ""
+      var remainingLength = length
+
+      while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+          var random: UInt8 = 0
+          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+          if errorCode != errSecSuccess {
+            fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+          }
+          return random
+        }
+
+        randoms.forEach { random in
+          if remainingLength == 0 {
+            return
+          }
+
+          if random < charset.count {
+            result.append(charset[Int(random)])
+            remainingLength -= 1
+          }
+        }
+      }
+
+      return result
     }
     
     /// - Tag: add_appleid_button
@@ -31,18 +93,19 @@ class LoginViewController: UIViewController {
     
     // - Tag: perform_appleid_password_request
     /// Prompts the user if an existing iCloud Keychain credential or Apple ID credential is found.
-    func performExistingAccountSetupFlows() {
-        // Prepare requests for both Apple ID and password providers.
-        let requests = [ASAuthorizationAppleIDProvider().createRequest(),
-                        ASAuthorizationPasswordProvider().createRequest()]
-        
-        // Create an authorization controller with the given requests.
-        let authorizationController = ASAuthorizationController(authorizationRequests: requests)
-        authorizationController.delegate = self
-        authorizationController.presentationContextProvider = self
-        authorizationController.performRequests()
-    }
+//    func performExistingAccountSetupFlows() {
+//        // Prepare requests for both Apple ID and password providers.
+//        let requests = [ASAuthorizationAppleIDProvider().createRequest(),
+//                        ASAuthorizationPasswordProvider().createRequest()]
+//
+//        // Create an authorization controller with the given requests.
+//        let authorizationController = ASAuthorizationController(authorizationRequests: requests)
+//        authorizationController.delegate = self
+//        authorizationController.presentationContextProvider = self
+//        authorizationController.performRequests()
+//    }
     
+//
     /// - Tag: perform_appleid_request
     @objc
     func handleAuthorizationAppleIDButtonPress() {
@@ -67,7 +130,6 @@ extension LoginViewController: ASAuthorizationControllerDelegate {
             // For the purpose of this demo app, store the `userIdentifier` in the keychain.
             self.saveUserInKeychain(userIdentifier)
             // For the purpose of this demo app, show the Apple ID credential information in the `ResultViewController`.
-            
         default:
             break
         }
@@ -91,7 +153,6 @@ extension LoginViewController: ASAuthorizationControllerPresentationContextProvi
 }
 
 extension UIViewController {
-    
     func showLoginViewController() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         if let loginViewController = storyboard.instantiateViewController(withIdentifier: "loginViewController") as? LoginViewController {
